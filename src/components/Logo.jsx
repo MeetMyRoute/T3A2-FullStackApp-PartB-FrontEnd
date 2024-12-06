d6689ad2efdb1ea342ddf5ff065ba32996ca4da0
kimnle <kimnle002@gmail.com>
2024-12-06 14:31:04 +1100

    completed logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+export function Logo() {
+    return(
+        <div className="logo">
+            <img src={logo} alt="Logo" />
+        </div>
+    )
+}
