4d621966fce599c2bcfda17f3c5afc0fabd60a49
theresaxn <hongoanh3@hotmail.com>
2024-12-11 01:06:06 +1100

    implemented profile context

--- src/contexts/ProfileContext.jsx
+++ src/contexts/ProfileContext.jsx
@@ -1,14 +1,59 @@
-import { createContext } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 
 export const defaultProfileData = {
-    name: "test",
-    location: "test",
-    status: "test",
-    profilePic: "test",
-    travelGoalsAndPreferences: "test",
-    socialMediaLink: "test"
-}
+    name: "",
+    location: "",
+    status: "",
+    profilePic: "",
+    travelGoalsAndPreferences: "",
+    socialMediaLink: ""
+};
 
-const ProfileContext = createContext(defaultProfileData);
+// Create the context
+const ProfileDataContext = createContext(defaultProfileData);
+const ProfileDataSetterContext = createContext(null);
 
-export default ProfileContext
+// Create custom hooks to access the context data
+export function useProfileContext() {
+    console.log("Passing data around");
+    return useContext(ProfileDataContext);
+};
+
+export function useProfileContextSetter() {
+    return useContext(ProfileDataSetterContext);
+};
+
+// Create the context provider
+export default function ProfileProvider(props) {
+    let [profileData, setProfileData] = useState(defaultProfileData);
+
+    let {userAuthData} = useUserAuthContext();
+
+    async function fetchProfileData(accessToken, userId) {
+        const result = await fetch(
+            `/profile/${userId}`, // need to fix
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            }
+        )
+        return await result.json();
+    };
+
+    useEffect(() => {
+        if (userAuthData && userAuthData.userJwt) {
+            fetchProfileData(userAuthData.userJwt)
+            .then(profileData => setProfileData(profileData));
+        }
+    }, [userAuthData]);
+
+    return (
+        <ProfileDataContext.Provider value={profileData}>
+            <ProfileDataSetterContext.Provider value={setProfileData}>
+                {props.children}
+            </ProfileDataSetterContext.Provider>
+        </ProfileDataContext.Provider>
+    )
+};
