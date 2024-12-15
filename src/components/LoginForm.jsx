885b9b53360dbeaaae7a3ca31ee691fe2a8460b2
sooffia <sofia23kabir@gmail.com>
2024-12-15 22:22:51 +1100

    change css file import

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-import './LoginForm.css';
+import '../stylesheets/LoginForm.css';
 
 // const API = import.meta.env.VITE_API_URL;  
 
