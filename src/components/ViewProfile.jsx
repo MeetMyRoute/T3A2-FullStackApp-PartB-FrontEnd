686c315a659fa3073a74b4589fbe7c373e510f69
theresaxn <hongoanh3@hotmail.com>
2024-12-15 16:44:48 +1100

    implemented ViewProfile component and stylesheet

---
+++ src/components/ViewProfile.jsx
@@ -0,0 +1,31 @@
+import "../stylesheets/ViewProfile.css"
+
+export default function ViewProfile({profileData}) {
+    if (profileData.name) {
+        return (
+            <div className="profileView">
+                <div className="profileViewLeft">
+                    <h1 id="viewName">{profileData.name}</h1>
+                    <h2 id="viewLocation">{profileData.location}</h2>
+                    {profileData.profilePic && <img src={profileData.profilePic} alt="Profile picture" id="viewProfilePic" />}
+                    <h3 id="viewStatus">{profileData.status}</h3>
+                </div>
+                <div className="profileViewRight">
+                    {profileData.travelPreferencesAndGoals.length > 0 ?
+                    <div className="viewTravelPrefAndGoals"> 
+                        <p>Travel Preferences & Goals:</p>
+                        <ul className="viewTravelPrefAndGoalsItem">{profileData.travelPreferencesAndGoals.map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}</ul>
+                    </div> :
+                    null}
+                    {profileData.socialMediaLink && <a id="viewSocialMedia"href={profileData.socialMediaLink}>{profileData.socialMediaLink}</a>} <br />
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <h3 className="error">Error in loading profile, please try again.</h3>
+        )
+    }
+}
