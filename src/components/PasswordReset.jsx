8c3614551e1d41d3266a167b3e19bc7359fb5d96
sooffia <sofia23kabir@gmail.com>
2024-12-15 22:22:23 +1100

    change css file import

--- src/components/PasswordReset.jsx
+++ src/components/PasswordReset.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "../stylesheets/PasswordReset.css";
 
 // const API = import.meta.env.VITE_API_URL;
 
