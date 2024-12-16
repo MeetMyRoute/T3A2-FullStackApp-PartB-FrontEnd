301702d8242b9d4e2b94bae58ec9ec0733f96c05
theresaxn <hongoanh3@hotmail.com>
2024-12-16 18:07:33 +1100

    added ProfileData context provider and amended profile path in Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ProfileProvider from './contexts/ProfileContext.jsx'
+import { ProfileDataProvider } from './contexts/ProfileContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ProfileProvider>
+    <ProfileDataProvider>
       <App />
-    </ProfileProvider>
+    </ProfileDataProvider>
   </StrictMode>
 )
