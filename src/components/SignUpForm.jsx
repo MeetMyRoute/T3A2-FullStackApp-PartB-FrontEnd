fe72b5ea4e7740fc46fb98e012a7d27159b40d6c
sooffia <sofia23kabir@gmail.com>
2024-12-20 16:19:35 +1100

    add missing / in API endpoint

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -70,7 +70,7 @@ export const SignUpForm = () => {
                 data.append("profilePic", formData.profilePic);
             }
     
-            const response = await axios.post(`${API}user/`, data, {
+            const response = await axios.post(`${API}/user/`, data, {
                 headers: {
                     "Content-Type": "multipart/form-data", 
                 },
