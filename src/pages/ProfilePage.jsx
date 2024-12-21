f99651ae73ade7287aacc40aaf6a79b5f031e872
theresaxn <hongoanh3@hotmail.com>
2024-12-21 16:44:09 +1100

    removed userId param

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,15 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ProfileContainer from "../components/ProfileContainer";
-import { useParams } from "react-router-dom";
 
 export function ProfilePage() {
-    const { userId } = useParams();
-
-    if (userId) {
-        return (
-            <div>
-                <ProfileContainer userId={userId} />
-            </div>
-        )
-    }
+    return (
+        <div>
+            <ProfileContainer />
+        </div>
+    )
 }
